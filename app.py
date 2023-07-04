6a39da295c369341b0df74b3a8147ac95c60b3c9
jcoop32 <jcooper11@hawk.iit.edu>
2023-07-04 08:57:21 -0500

    instructions

---
+++ app.py
@@ -0,0 +1,23 @@
+# things we need:
+
+# if user wants to add, edit, or delete a new task
+# if add -> ask what they want to add -> add to list which has a number assigned to it 
+# if edit -> ask for which number of task wanted to change -> prompt for new entry
+# if delete -> ask for which number of task -> remove from list
+# list tasks
+# exit program
+# complete a task
+
+# display a list of menu options
+# user will select an option
+# add- run addTask() -> return to menu
+# edit - run editTask() -> return to menu
+# delete - run deleteTask() -> return to menu
+# complete - run completeTask() - return to menu
+# exit - break loop
+# read the input and call function 
+# return to menu
+
+# completeTask() -> if isDone = false -> mark complete
+
+
